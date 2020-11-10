7651743be0dfc2526ef5c9983db09a7544ca7e50
Craig5117 <craig.bennett5117@gmail.com>
2020-11-10 01:07:52 -0500

    made some progress with the goal array function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,36 @@
-var newStudentName = function(){
-    
-    console.log("New student");
+var currentStudent = "";
+var studentsArray = [];
+var studentsGoalsArray = []
+var goalsArray = [{goal: "Yes/No Questions", description: "This is a description of Yes/No questions."}, {goal: "When Questions", description: "This is a description of When questions."}];
+ 
+var studentGoalUpdate = function () {
+    var currentStudentArray = [];
+    $(".goal").each(function(){
+        var newEvalArray = [];
+        var goalName = $(this).children("h2").text();
+        console.log("Goal name is: " + goalName);
+        $(".goal-eval").each(function(){
+            var evalDate = $(this).children("p").text();
+            console.log(evalDate);
+            var scoreContainerEl = $(this).children("div");
+            var evalScore = scoreContainerEl.children("p").text();
+            // var evalScore = $(this).find(".score").text();
+            console.log("EvalScore is: " + evalScore);
+            var scorePercent = $(this).children(".scorePercent").text();
+            console.log("ScorePercent is: " + scorePercent);
+            var evalObj = {date: evalDate, score: evalScore, percent: scorePercent};
+            console.log("This is the evalObj: " + evalObj);
+            newEvalArray.push(evalObj);
+            console.log("This is the newEvalArray" + newEvalArray);
+        })
+        var goalDataObj = {goal: goalName, eval: newEvalArray}
+        currentStudentArray.push(goalDataObj);
+        console.log("This is the currentStudentArray: " + currentStudentArray);
+    })
+    studUpdateArrayData = {student: currentStudent, data: currentStudentArray}
+    studentsGoalsArray.push(studUpdateArrayData);
+    console.log("This is the updated studentGoalsArray: " + studentsGoalsArray);
 }
-
-var newGoal = function(){
-    console.log("New goal");
-};
-
-
 var displayStudentGoals = function(currentStudent) {
     $(".goal").each(function(){
         $(this).remove();
@@ -37,19 +60,26 @@ $("#student-form-modal .btn-save").on("click", function(){
 });
 
 $("#student-name").on("change", function(){
-    var currentStudent = $("#student-name option:selected").text();
+    currentStudent = $("#student-name option:selected").text();
     displayStudentGoals(currentStudent);
 });
 
 $("#goal-form-modal").on("show.bs.modal", function() {
     // clear values
-    $("#modalGoalTitle, #modalGoalDescription, modalDate").val("");
+    $("#modalGoalTitle, #modalGoalDescription").val("");
 });
 
 $("#goal-form-modal").on("shown.bs.modal", function() {
     // highlight textarea
     $("#modalGoalTitle").trigger("focus");
 });
+
+$("#goal-select").on("change", function(){
+    currentGoal = $("#goal-select option:selected").text();
+
+});
+
+$("#newEvalBtn").on("click", studentGoalUpdate);
 
 // // $("#newStudentBtn").on("click", newStudentName);
 // $("#newGoalBtn").on("click", newGoal);
