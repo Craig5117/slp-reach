e7ce9d8e584e682f3fad3a9d3265ea1d5b193923
Craig5117 <craig.bennett5117@gmail.com>
2020-11-09 19:48:05 -0500

    students added to select list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var newStudentName = function(){
+    
     console.log("New student");
 }
 
@@ -6,5 +7,39 @@ var newGoal = function(){
     console.log("New goal");
 };
 
-$("#newStudentBtn").on("click", newStudentName);
-$("#newGoalBtn").on("click", newGoal);
+
+var displayStudentGoals = function(currentStudent) {
+    $(".goal").each(function(){
+        $(this).remove();
+    })
+}
+
+$("#student-form-modal").on("show.bs.modal", function() {
+    // clear values
+    $("#nameEntry").val("");
+});
+
+$("#student-form-modal").on("shown.bs.modal", function() {
+    // highlight textarea
+    $("#nameEntry").trigger("focus");
+});
+
+$("#student-form-modal .btn-save").on("click", function(){
+    var studentName = $("#nameEntry").val().trim();
+    if (studentName) {
+        var studentOptionEl = $("<option>").attr("value", studentName);
+        studentOptionEl.text(studentName);
+        $("#student-name").append(studentOptionEl);
+        $("#student-form-modal").modal("hide");
+    }
+   // Save name to array code here
+
+});
+
+$("#student-name").on("change", function(){
+    var currentStudent = $("#student-name option:selected").text();
+    displayStudentGoals(currentStudent);
+})
+
+// // $("#newStudentBtn").on("click", newStudentName);
+// $("#newGoalBtn").on("click", newGoal);
