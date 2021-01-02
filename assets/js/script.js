44c216a793067f77b20c4cc4caf0795c637c39ce
Craig5117 <craig.bennett5117@gmail.com>
2021-01-02 13:34:28 -0500

    goals array bug fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,19 +33,23 @@ const loadStudentsList = function () {
 
 // gets list of goals and their descriptions from localStorage
 const loadGoalsData = function () {
-  goalsArray = localStorage.getItem("mySLPReachGoals");
-  goalsArray = JSON.parse(goalsArray);
-  if (!goalsArray) {
-    return goalsArray = [{ goal: "Yes No Questions", description: "This is a description of Yes/No questions." }, { goal: "When Questions", description: "This is a description of When questions." }, { goal: "Categories", description: "This is a description of categories." }];
-  } else {
-    for (let i = 0; i < goalsArray.length; ++i) {
-      let goalOptionEl = $("<option>").attr("value", goalsArray[i].goal);
-      goalOptionEl.text(goalsArray[i].goal);
-      $("#goal-select").append(goalOptionEl);
+    goalsArray = localStorage.getItem("mySLPReachGoals");
+    goalsArray = JSON.parse(goalsArray);
+    const loadGoals = function () {
+        for (let i = 0; i < goalsArray.length; ++i) {
+            let goalOptionEl = $("<option>").attr("value", goalsArray[i].goal);
+            goalOptionEl.text(goalsArray[i].goal);
+            $("#goal-select").append(goalOptionEl);
+        }
+    };
+    if (!goalsArray) {
+        goalsArray = [{goal: "Yes No Questions", description: "This is a description of Yes/No questions."}, {goal: "When Questions", description: "This is a description of When questions."}, {goal: "Categories", description: "This is a description of categories."}];
+        updateGoalsData();
+        loadGoals();
+        return; 
     }
-    updateGoalsData();
-  }
-};
+    loadGoals();
+};    
 
 // sets localStorage of studentsGoalsArray
 const updateStudentData = function () {
